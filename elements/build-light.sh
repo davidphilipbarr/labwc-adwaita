7344205b2036b48711ae252559c34d2756ba242b
David Barr <dpbarr@gmail.com>
2024-11-25 15:09:30 +0000

    fix Directory nonexistent error

--- elements/build-light.sh
+++ elements/build-light.sh
@@ -2,7 +2,7 @@ themename="Adwaita"
 mkdir -p ../themes/$themename/openbox-3
 
 rm ../themes/$themename/openbox-3/themerc 
-echo "$(cat ./src/borders-light)" >> ../$themename/openbox-3/themerc
+echo "$(cat ./src/borders-light)" >> ../themes/$themename/openbox-3/themerc
 echo "$(cat ./src/common)" >> ../themes/$themename/openbox-3/themerc
 echo "$(cat ./src/title-light)" >> ../themes/$themename/openbox-3/themerc
 echo "$(cat ./src/menu-light)" >> ../themes/$themename/openbox-3/themerc
