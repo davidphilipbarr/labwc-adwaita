7517189aca4b798b367dbf85752ead5ff4eaf2cc
David Barr <dpbarr@gmail.com>
2024-12-01 15:26:02 +0000

    add openbox compatability for xbm themes

--- elements/build-dark-xbm.sh
+++ elements/build-dark-xbm.sh
@@ -6,7 +6,7 @@ echo "$(cat ./src/common)" >> ../themes/$themename/openbox-3/themerc
 echo "$(cat ./src/title-dark)" >> ../themes/$themename/openbox-3/themerc
 echo "$(cat ./src/menu-dark)" >> ../themes/$themename/openbox-3/themerc
 echo "$(cat ./src/osd-dark)" >> ../themes/$themename/openbox-3/themerc
-
+echo "$(cat ./src/openbox-dark)" >> ../themes/$themename/openbox-3/themerc
 cp ./buttons-bitmap/*.xbm ../themes/$themename/openbox-3/
 
 cp -r ../themes/$themename ~/.local/share/themes/
